4219f79254ea3a935f274acc6ead5905d4954346
mirac <mirac.i.sancak@gmail.com>
2026-02-27 21:12:11 +0100

    History und Echo hinzugefügt

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include "shell.h"
 
+char *myhistory[MAX_HISTORY];
+int history_count = 0;
+
 int main() {
     char *args[MAX_ARGS];
     char cwd[1024];
@@ -16,7 +19,11 @@ int main() {
         // Input lesen
         char *input = readline(prompt);
         if (input == NULL) break;
-        if (strlen(input) > 0) add_history(input);
+
+        if (strlen(input) > 0) {
+            add_history(input);
+            myhistory[history_count++] = strdup(input);
+        }
 
         // Pipe check
         int anzahl = split_pipes(input, befehle);
@@ -54,6 +61,30 @@ int main() {
                     execute_redirect(args, datei, type);
                 else
                     my_ls(args[1]);
+                free(input);
+                continue;
+            }
+
+            // history
+            if (strcmp(args[0], "history") == 0) {
+                for (int i = 0; i < history_count; i++) {
+                    printf("%d  %s\n", i + 1, myhistory[i]);
+                }
+                free(input);
+                continue;
+            }
+
+            if (strcmp(args[0], "echo") == 0) {
+                for (int i = 1; i < anzahl_args; i++) {
+                    if (args[i][0] == '$') {
+                        char *varname = args[i] + 1;
+                        char *value = getenv(varname);
+                        if (value) printf("%s ", value);
+                    } else {
+                        printf("%s ", args[i]);
+                    }
+                }
+                printf("\n");
                 free(input);
                 continue;
             }
